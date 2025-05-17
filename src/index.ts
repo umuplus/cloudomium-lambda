111c79be16edb1eb74e21dc85db3370b0b0b4b75
Umut Aydin <aydinumut82@gmail.com>
2025-05-17 16:26:16 +0300

    minor fixes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import cacheMiddleware from './middlewares/cache'
 import callbackWaitsForEmptyEventLoopMiddleware from './middlewares/callback-waits-for-empty-event-loop'
 import corsMiddleware from './middlewares/cors'
 import jsonBodyParserMiddleware from './middlewares/json-body-parser'
+import languageMiddleware from './middlewares/language'
 import authMiddleware from './middlewares/auth'
 import httpValidatorMiddleware from './middlewares/http-validator'
 import s3FetchMiddleware from './middlewares/s3-fetch'
@@ -16,10 +18,12 @@ export * from './types/s3'
 export * from './types/sqs'
 
 export {
+    authMiddleware,
+    cacheMiddleware,
     callbackWaitsForEmptyEventLoopMiddleware,
     corsMiddleware,
-    jsonBodyParserMiddleware,
-    authMiddleware,
     httpValidatorMiddleware,
+    jsonBodyParserMiddleware,
+    languageMiddleware,
     s3FetchMiddleware,
 }
